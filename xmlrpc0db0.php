c91e3a540cecc9ed00239414b31e01795579a372
Miguel Gargallo <mig898@g.harvard.edu>
2022-03-05 15:44:04 +0100

    B 1.0 Static
     Static Site

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://bioverse.app</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://bioverse.app/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://bioverse.app/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://bioverse.app/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://bioverse.app/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://bioverse.app/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
